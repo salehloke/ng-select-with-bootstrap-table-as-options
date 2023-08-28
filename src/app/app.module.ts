e44ffce3e6ca4aac5df37d1c1a2cfe45caf4878e
salehloke <58175520+salehloke@users.noreply.github.com>
2023-08-29 07:34:07 +0800

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,21 @@ import { HelloComponent } from './hello.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CommonModule } from '@angular/common';
 import { NgSelectModule } from '@ng-select/ng-select';
+import { NgOptionHighlightModule } from '@ng-select/ng-option-highlight';
+import { BasicFormComponent } from './basic-form/basic-form.component';
 
 @NgModule({
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [
     BrowserModule,
+    NgSelectModule,
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
     AppRoutingModule,
-    NgSelectModule,
+    NgOptionHighlightModule,
   ],
-  declarations: [AppComponent, HelloComponent],
+  declarations: [AppComponent, BasicFormComponent],
   bootstrap: [AppComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {}
